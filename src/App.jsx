3025442b3be7862d8a1e5fe0a635c03d6e9ee4ba
Manash04 <manash9787@gmail.com>
2024-07-03 23:27:03 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Accordian from './components/accordianFolder'
+
+function App() {
+
+  return (
+   <div>
+<Accordian/>
+   </div>
+  )
+}
+
+export default App
