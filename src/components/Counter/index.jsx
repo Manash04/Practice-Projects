decae235fa3aff2823f9094297891699aea446ae
Manash04 <manash9787@gmail.com>
2024-07-04 10:02:56 +0530

    Counter App

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-  function handleChange() {
-    setCount(prevCOunt => prevCOunt + 1);
+  function increment(){
+    setCount(count + 1);
+  }
+  function decrement(){
+    setCount(count - 1);
   }
   return (
     <div>
-      <button onClick={handleChange}>Count {count}</button>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
     </div>
   );
 };
